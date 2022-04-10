b0f907498b8bf7b2f8d047021ef5cba33eb0543f
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-04-11 01:14:14 +0600

    linear search algorithm added

---
+++ LinearSearch/LinearSearch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LinearSearch {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        // loop for user input
+        for(int i = 0; i<size; i++){
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println("Please Enter the value to find: ");
+        int x = sc.nextInt();
+        // loop for traversing the array
+        for(int i = 0; i<numbers.length; i++){
+            if(numbers[i] == x){
+                System.out.println("x found at index: "+ i );
+            }
+
+        }
+
+    }
+}
