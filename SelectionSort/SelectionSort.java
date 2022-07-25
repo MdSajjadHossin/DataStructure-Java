87788ebfb1e74ef5d734f38b55972f3136b22229
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-07-25 22:00:11 +0600

    Selection Sort added

---
+++ SelectionSort/SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class SelectionSort{
+    public static void printArray(int arr[]) {
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]+ "");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {9, 13, 15, 11, 2};
+
+        //selection sort
+        for(int i = 0; i < arr.length -1; i++){
+            int smallest = i;
+            //compearing the smallest
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
