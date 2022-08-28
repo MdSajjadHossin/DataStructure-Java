dd7be659abec1a49239afab2881194f27dc516a7
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-08-28 07:13:17 +0600

    array list adding operation added

---
+++ ArrayList/ArrayLists.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class ArrayLists{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        //add element
+        list.add(4);
+        list.add(5);
+        list.add(10);
+
+        System.out.println(list);
+
+    }
+}
