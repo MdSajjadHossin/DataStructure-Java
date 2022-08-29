1bc938ad7e1ab00f193b51eea8d44b97551ccafe
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-08-29 07:45:38 +0600

    Linked list added

---
+++ LinkdList/LinkedList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class LinkdList{
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+
+
+        //addition
+        list.addFirst("a");
+        list.addFirst("b");
+
+        System.out.println(list);
+
+    }
+}
