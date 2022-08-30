be3a839f878896f427285ac3acb33560ebad0eab
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-08-30 09:10:04 +0600

    merge sort added

--- MergeSort/MergeSort.java
+++ MergeSort/MergeSort.java
@@ -1,14 +1,39 @@
 public class MergeSort{
     public static void conqure(int arr[], int start, int mid, int end) {
-        
+        int merged[] = new int[end - start + 1];
+
+        int index1 = start;
+        int index2 = mid+1;
+        int x = 0;
+
+        while(index1 <= mid && index2 <= end){
+            if(arr[index1] <= arr[index2]){
+                merged[x++] = arr[index1++];
+                // x++; index1++;
+            }else{
+                merged[x++] = arr[index2++];
+
+            }
+        }
+        while(index1 <= mid){
+            merged[x++] = arr[index1++];
+        }
+        while(index2 <= end){
+            merged[x++] = arr[index2++];
+        }
+
+        //puting the value into origenal array
+        for(int i = 0, j=start; i<merged.length; i++, j++){
+            arr[j] = merged[i]; 
+        }
     }
-    public static void divide(int arr[], int start, int mid, int end){
+    public static void divide(int arr[], int start, int end){
         if(start >= end){
             return;
         }
 
         // mid = (start + end)/2;
-        mid = start + (end - start)/2;
+        int mid = start + (end - start)/2;
         divide(arr, start, mid);
         divide(arr, mid+1, end);
         conqure(arr, start, mid, end);
@@ -16,5 +41,12 @@ public class MergeSort{
     public static void main(String[] args) {
         int arr[] = {6, 3, 9, 5, 2, 8};
         int n = arr.length;
+
+        divide(arr, 0, n-1);
+        //print
+        for(int i =0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
